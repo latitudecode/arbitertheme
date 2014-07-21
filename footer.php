9acf9bd379717aa1214bbd6ce09d474e141bc1f5
Sam Baker-Salmon <sambakersalmon@Sams-MacBook-Pro.local>
2014-07-20 21:31:34 -0400

    adding bits and pieces to footer header and index

--- footer.php
+++ footer.php
@@ -1,8 +1,33 @@
 	<footer>
-the footer
-        <?php wp_footer(); ?>
+	
+    <div class="row">
+
+                <div class="medium-4 columns">
+                    <h4><a href="#">info@arbiterproductions</a></h2>
+                    <h4><a href="#">917.443.2211</a></h2>
+                </div>
+
+
+                <div class="medium-4 columns">
+                    <h4><a href="#">twitter</a></h2>
+                    <h4><a href="#">facebook</a></h2>
+                    <h4><a href="#">instagram</a></h2>
+                </div>
+
+                <div class="medium-4 columns">
+                    <h4 style="color: #fff; margin-top: 15%;">Arbiter Productions</h4>
+                </div>
+    </div>
 
 	</footer>
+	<div class="small-foot">
+	</div>
+
+</div>
+
+<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/cbpAnimatedHeader.js"></script>
+
+<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/classie.js"></script>
 
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,5 +1,139 @@
 <?php get_header(); ?>
 
-The content
+
+<div class="stay-put"> <!-- bio content -->
+                    <video width="530" height="400" muted="" loop="none" autoplay="none" id="video">
+                        <source type="video/ogg" src="http://workbysam.com/video/deys.ogg" preload="none"></source>
+                    </video>
+ <!-- starting the WordPress loop -->
+	
+	<div class="row">
+  		<div class="medium-12 columns">
+  			<h1><a href="page-about.html">Arbiter Productions</a> is a multimedia production company specializing in <a href="page-projects.html">video content</a> for visual artists</h1>
+            <ul class="call-two-action">
+                <li><a href="page-about.html">About Us</a></li>
+                <li><a href="page-projects.html">Projects</a></li>
+            </ul>
+
+
+  		</div>
+	</div>
+</div>
+
+
+ <!-- end loop -->
+
+<div class="stage"> <!-- curtain pull -->
+    <div class="filter">
+        <div class="row">
+            <ul class="filter-nav">
+                <a href="http://www.google.com"><li class="button float">Branded Content</li></a>
+                <li class="button float">Documentaries</li>
+                <li class="button float">Narrative</li>
+            </ul>
+        </div>
+    </div>      
+
+	<div class="wrap"> <!-- start wrap -->
+        <div class="row"> <!-- start row -->
+
+            <!-- starting the WordPress loop -->               
+
+                <div class="medium-12 columns">
+                    <a href="page-video.html"> <!-- link wrap -->
+                        <div class="view view-tenth">
+                            <iframe src="//player.vimeo.com/video/27353914" width="500" height="281" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+                        </div>
+                    </a> <!-- link end wrap -->
+                </div>
+
+
+             <!-- starting the WordPress loop -->               
+
+                <div class="medium-6 columns">
+                    <a href="page-video.html"> <!-- link wrap -->
+                        <div class="view view-tenth">
+                            <div class="hover-effect"><h2>Dominican Travel Network</h2></div>
+                            <img width="400" height="236" src="images/thumb.jpg" class="attachment-post-thumbnail wp-post-image" alt="turtle-talk-thumb" />
+                        </div>
+                    </a> <!-- link end wrap -->
+                </div>
+
+             <!-- starting the WordPress loop -->
+
+                
+                <div class="medium-6 columns">
+                    <a href="page-video.html"> <!-- link wrap -->
+                        <div class="view view-tenth">
+                            <div class="hover-effect"><h2>Dominican Travel Network</h2></div>                            
+                            <img width="400" height="236" src="images/thumb.jpg" class="attachment-post-thumbnail wp-post-image" alt="the-frog-thumb" />
+                        </div>
+                    </a> <!-- link end wrap -->
+                </div>
+
+             <!-- starting the WordPress loop -->
+
+                
+                <div class="medium-6 columns">
+                    <a href="page-video.html"> <!-- link wrap -->
+                        <div class="hover-effect"><h2>Dominican Travel Network</h2></div>                        
+                        <div class="view view-tenth">
+                            <img width="400" height="236" src="images/thumb.jpg" class="attachment-post-thumbnail wp-post-image" alt="shortcomings-thumb" />
+                        </div>
+                    </a> <!-- link end wrap -->
+                </div>
+
+             <!-- starting the WordPress loop -->
+                
+
+                <div class="medium-6 columns">
+                    <a href="page-video.html"> <!-- link wrap -->
+                        <div class="hover-effect"><h2>Dominican Travel Network</h2></div>                        
+                        <div class="view view-tenth">
+                            <img width="400" height="220" src="images/thumb.jpg" class="attachment-post-thumbnail wp-post-image" alt="a-moose-thumb" />
+                        </div>
+                    </a> <!-- link end wrap -->
+                </div>
+
+             <!-- starting the WordPress loop -->
+
+            
+                <div class="medium-6 columns">
+                    <a href="page-video.html"> <!-- link wrap -->
+                        <div class="hover-effect"><h2>Dominican Travel Network</h2></div>                        
+                        <div class="view view-tenth">
+                            <img width="400" height="236" src="images/thumb.jpg" class="attachment-post-thumbnail wp-post-image" alt="worm-problems-thumb" />
+                        </div>
+                    </a> <!-- link end wrap -->
+                </div>
+
+             <!-- starting the WordPress loop -->
+
+
+                <div class="medium-6 columns">
+                    <a href="page-video.html"> <!-- link wrap -->
+                        <div class="hover-effect"><h2>Dominican Travel Network</h2></div>
+                        <div class="view view-tenth">
+                            <img width="400" height="236" src="images/thumb.jpg" class="attachment-post-thumbnail wp-post-image" alt="woman-heels-thumb" />
+                        </div>
+                    </a> <!-- link end wrap -->
+                </div>
+
+             <!-- starting the WordPress loop -->
+
+  		</div> <!-- end row -->
+
+  	</div> <!-- end wrap -->
+
+<div class="wrap">
+
+ 
+</div>
+</div>
+</div>
+</div>
+
+
+</div>
 
 <?php get_footer(); ?>
