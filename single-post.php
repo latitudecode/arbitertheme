3585f6e8160a62f629bcc2a993e3552b221f7ecd
Sam Baker-Salmon <sambakersalmon@Sams-MacBook-Pro.local>
2014-07-21 21:52:51 -0400

    adding loop to single posts

--- single-post.php
+++ single-post.php
@@ -8,7 +8,18 @@
         <div class="medium-12 columns">
             <a href="#"> <!-- link wrap -->
                 <div class="view view-tenth">
-                    <iframe src="//player.vimeo.com/video/27353914" width="500" height="281" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+				<?php
+					// Get the video URL and put it in the $video variable
+					$videoID = get_post_meta($post->ID, 'video_url', true);
+					// Check if there is in fact a video URL
+					if ($videoID) {
+						echo '<div class="videoContainer">';
+						// Echo the embed code via oEmbed
+						echo wp_oembed_get( 'http://vimeo.com/' . $videoID ); 
+						echo '</div>';
+					}
+					?>
+
                 </div>
             </a> <!-- link end wrap -->
         </div>
@@ -40,74 +51,27 @@
 	<div class="wrap"> <!-- start wrap -->
         <div class="row"> <!-- start row -->            
 
+            <?php query_posts(''); if(have_posts()) : while(have_posts()) : the_post(); ?> <!-- starting the WordPress loop -->
+
                 <div class="medium-6 columns">
-                    <a href="page-video.html"> <!-- link wrap -->
+                    <a href="<?php the_permalink(); ?>"> <!-- link wrap -->
                         <div class="view view-tenth">
-                            <div class="hover-effect"><h2>Dominican Travel Network</h2></div>
-                            <img width="400" height="236" src="images/thumb.jpg" class="attachment-post-thumbnail wp-post-image" alt="turtle-talk-thumb" />
+                            <div class="hover-effect"><h2><?php the_title(); ?></h2></div>
+                            <?php 
+                            if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
+                              the_post_thumbnail();
+                            } 
+                            ?>
                         </div>
                     </a> <!-- link end wrap -->
                 </div>
 
-             <!-- starting the WordPress loop -->
+            <?php endwhile; ?>
 
-                
-                <div class="medium-6 columns">
-                    <a href="page-video.html"> <!-- link wrap -->
-                        <div class="view view-tenth">
-                            <div class="hover-effect"><h2>Dominican Travel Network</h2></div>                            
-                            <img width="400" height="236" src="images/thumb.jpg" class="attachment-post-thumbnail wp-post-image" alt="the-frog-thumb" />
-                        </div>
-                    </a> <!-- link end wrap -->
-                </div>
+            <?php else : ?>
+                <p>Whoops! We are working on it.</p>
 
-             <!-- starting the WordPress loop -->
-
-                
-                <div class="medium-6 columns">
-                    <a href="page-video.html"> <!-- link wrap -->
-                        <div class="hover-effect"><h2>Dominican Travel Network</h2></div>                        
-                        <div class="view view-tenth">
-                            <img width="400" height="236" src="images/thumb.jpg" class="attachment-post-thumbnail wp-post-image" alt="shortcomings-thumb" />
-                        </div>
-                    </a> <!-- link end wrap -->
-                </div>
-
-             <!-- starting the WordPress loop -->
-                
-
-                <div class="medium-6 columns">
-                    <a href="page-video.html"> <!-- link wrap -->
-                        <div class="hover-effect"><h2>Dominican Travel Network</h2></div>                        
-                        <div class="view view-tenth">
-                            <img width="400" height="220" src="images/thumb.jpg" class="attachment-post-thumbnail wp-post-image" alt="a-moose-thumb" />
-                        </div>
-                    </a> <!-- link end wrap -->
-                </div>
-
-             <!-- starting the WordPress loop -->
-
-            
-                <div class="medium-6 columns">
-                    <a href="page-video.html"> <!-- link wrap -->
-                        <div class="hover-effect"><h2>Dominican Travel Network</h2></div>                        
-                        <div class="view view-tenth">
-                            <img width="400" height="236" src="images/thumb.jpg" class="attachment-post-thumbnail wp-post-image" alt="worm-problems-thumb" />
-                        </div>
-                    </a> <!-- link end wrap -->
-                </div>
-
-             <!-- starting the WordPress loop -->
-
-
-                <div class="medium-6 columns">
-                    <a href="page-video.html"> <!-- link wrap -->
-                        <div class="hover-effect"><h2>Dominican Travel Network</h2></div>
-                        <div class="view view-tenth">
-                            <img width="400" height="236" src="images/thumb.jpg" class="attachment-post-thumbnail wp-post-image" alt="woman-heels-thumb" />
-                        </div>
-                    </a> <!-- link end wrap -->
-                </div>
+            <?php endif; ?> <!-- end loop -->
 
              <!-- starting the WordPress loop -->
 
